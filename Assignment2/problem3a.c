f5827840549c32cf08c869a93b1196280212bd58
nikolapeevski <Jeratull@hotmail.com>
2019-02-05 23:31:59 -0600

    (type:chore) Configure CMAKE Update gitignore

--- Assignment2/problem3a.c
+++ Assignment2/problem3a.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <printf.h>
 
-int main() {
+void generateFeature(int size) {
+    printf("%d", size);
+}
+void generateMatrix(int size) {
+    printf("%d", size);
+}
 
+int main(int argc, char *argv[]) {
+    if (argv[1] == NULL || argv[2] == NULL) {
+        printf("Wrong input %c, %c", *argv[1], *argv[2]);
+        return 0;
+    }
+    printf("%c\n", *argv[1]);
+    generateMatrix((*argv[1] - '0'));
+
+    return 0;
 }
